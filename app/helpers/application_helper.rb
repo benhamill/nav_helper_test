9c54798d4145ac6e311dcf2a4fb07f4daad693f4
Ben Hamill <git-commits@benhamill.com>
2012-12-21 23:22:34 -0600

    Make the top and side nas highlight right.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def controller_classes
-    "settings"
+    path_elements = controller_path.split('/')
+    top_nav = path_elements.first
+    side_nav = path_elements.last
+
+    "#{top_nav} sub_#{side_nav}"
   end
 end
